e7cc67fe1e9ccc9fa5b9a65635da4cd263c37c49
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-12-06 20:36:31 -0500

    added home page skeleton

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,25 @@
+
+function Home() {
+    return (
+        <main>
+            <h1>Home Page</h1>
+            <img src="https://placedog.net/250" alt="profile pic" />
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Genre</li>
+                    <li>Bands</li>
+                </ul>
+            </nav>
+            <div>
+                <h2>Friends</h2>
+            </div>
+            <div>
+                <h2>Main Feed</h2>
+            </div>
+            <div>
+                <h2>Chats</h2>
+            </div>
+        </main>
+    )
+}
