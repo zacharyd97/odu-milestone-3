5d709dea5094be5e44f0853da9e0bf90b30b1335
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-12-08 20:36:23 -0500

    added genre associations

--- backend/models/genre.js
+++ backend/models/genre.js
@@ -9,8 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate(User) {
+      genre.belongsToMany(User, {
+        through: 'Genre_Users'
+      })
     }
   }
   Genre.init({
@@ -20,6 +22,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Genre',
+    tableName: 'genre',
+    timestamps: false
   });
   return Genre;
 };
