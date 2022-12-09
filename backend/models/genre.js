7cbd4e94a99019ff53ff0cd50beeef331a453b0e
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-12-08 21:10:26 -0500

    fixed genre associations

--- backend/models/genre.js
+++ backend/models/genre.js
@@ -9,14 +9,25 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(User) {
-      genre.belongsToMany(User, {
-        through: 'Genre_Users'
+    static associate(User, Genre_Users, Post) {
+      Genre.belongsToMany(User, {
+        foreignKey: "user_id",
+        as: "users",
+        through: Genre_Users
+      })
+
+      Genre.hasMany(Post, {
+        foreignKey: "genre_id",
+        as: "posts"
       })
     }
   }
   Genre.init({
-    genre_id: DataTypes.INTEGER,
+    genre_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     name: DataTypes.STRING,
     description: DataTypes.STRING
   }, {
