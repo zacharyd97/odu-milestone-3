66cc9b0245891ed19f0843c5364271651d91c567
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-12-11 11:16:47 -0500

    changed junction table

--- backend/migrations/20221209011636-create-genre-users.js
+++ backend/migrations/20221209011636-create-genre-users.js
@@ -3,16 +3,18 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Genre_Users', {
-      user_id: {
+      genre_user_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
       genre_id: {
         allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
         type: Sequelize.INTEGER
       },
     });
