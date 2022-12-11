21881d2176514db0a873a3ca8da78d4625386b5a
Owen Andre <owencandre@gmail.com>
2022-12-11 14:47:25 -0500

    added data in seeders

--- backend/migrations/20221209011529-create-genre.js
+++ backend/migrations/20221209011529-create-genre.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Genres', {
+    await queryInterface.createTable('genres', {
       genre_id: {
         allowNull: false,
         autoIncrement: true,
--- backend/seeders/20221209003910-posts.js
+++ backend/seeders/20221209003910-posts.js
@@ -2,24 +2,17 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('posts', [{
+      user_id: 2,
+      genre_id: 1,
+      title: 'Place Holder',
+      post_text: 'This is a place holder post!'
+    }
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('posts', null, {});
   }
 };
